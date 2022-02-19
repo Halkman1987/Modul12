c7d9f3672f439828cd78601fe9d12b632318ace4
Halkman <halk-man@mail.ru>
2022-02-19 17:22:30 +0500

    new

--- Modul12/Program.cs
+++ Modul12/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Modul12
+{
+    class Programm
+    {
+        static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            // Остановка на 1 с
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            // Остановка на 2 с
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            // Остановка на 3 с
+            Thread.Sleep(3000);
+        }
+        static void Main()
+        {
+            List<User> users = new List<User>();
+            users.Add(new User() { Name = "Dima", IsPremium = false });
+            users.Add(new User() { Name = "Petya", IsPremium = false });
+            users.Add(new User() { Name = "Vasya", IsPremium = true });
+            foreach (User user in users)
+            {
+                if (user.IsPremium == false)
+                {
+                    Console.WriteLine($"Hello {user.Name}");
+                    ShowAds();
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine($"Hello {user.Name} you have is Premium account");
+                    return;
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ Modul12/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul12
+{
+    class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+    }
+}
